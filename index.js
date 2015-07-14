5752023d680e7c61ce56c5c416e9c8b790ebb9ef
mcberros <mcberros@gmail.com>
2015-07-14 19:48:00 +0200

    Added views

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/categories', function (req, res) {
+  res.render('categories');
+});
+
+app.use(function(req, res, next){ res.status(404);
+  res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){ console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
