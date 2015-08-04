240a670564e0d45d8f00bf8c38f1d77bc80afb9a
mcberros <mcberros@gmail.com>
2015-08-04 20:29:10 +0200

    Refactor: Created a server file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //Connection to DB
 var mongoose = require('mongoose');
+var startServer = require('./server');
+
 mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
@@ -8,43 +10,6 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function () {
 	console.log("connection to mongo opened");
+	startServer();
 });
 
-//App
-var express = require('express');
-var app = express();
-var methodOverride = require('method-override');
-
-var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
-
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-
-app.use(require('body-parser').urlencoded({extended: false}));
-
-app.use(methodOverride(function(req, res){
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method;
-    delete req.body._method;
-    return method;
-  }
-}))
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.use('/categories', require('./routes/category'));
-
-app.use(function(req, res, next){
-	res.status(404);
-  res.render('404');
-});
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
