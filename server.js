13ec4167fc44a9ce47326a44d13a178a44438209
mcberros <mcberros@gmail.com>
2015-08-13 20:19:28 +0200

    Changed port od server

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ module.exports = function(){
     res.render('404');
   });
 
-  var server = app.listen(3000, function () {
+  var server = app.listen(process.env.PORT || 3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
