08822b7bcc78a069e0e327768415dd0033331d03
mcberros <mcberros@gmail.com>
2015-08-17 19:17:53 +0200

    Added authentication

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+var credentials = require('./controllers/credentials');
+var flash = require('connect-flash');
+var passport = require('passport');
+var initPassport = require('./passport/init');
+
+
+var isAuthenticated = function (req, res, next) {
+  // if user is authenticated in the session, call the next() to call the next request handler 
+  // Passport adds this method to request object. A middleware is allowed to add properties to
+  // request and response objects
+  if (req.isAuthenticated())
+    return next();
+  // if the user is not authenticated then redirect him to the login page
+  res.redirect('/');
+}
 
 module.exports = function(){
   var app = express();
@@ -8,7 +23,11 @@ module.exports = function(){
   app.engine('handlebars', handlebars.engine);
   app.set('view engine', 'handlebars');
 
+  app.use(require('cookie-parser')(credentials.cookieSecret));
   app.use(require('body-parser').urlencoded({extended: false}));
+  app.use(require('express-session')());
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   app.use(methodOverride(function(req, res){
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
@@ -19,8 +38,39 @@ module.exports = function(){
     }
   }))
 
+  app.use(flash());
+
+  initPassport(passport);
+
   app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('index', { message: req.flash('message') });
+  });
+
+  app.post('/login', passport.authenticate('login', {
+    successRedirect: '/home',
+    failureRedirect: '/',
+    failureFlash : true
+  }));
+
+  /* GET Registration Page */
+  app.get('/signup', function(req, res){
+    res.render('register', {message: req.flash('message')});
+  });
+
+  /* Handle Registration POST */
+  app.post('/signup', passport.authenticate('signup', {
+    successRedirect: '/home',
+    failureRedirect: '/signup',
+    failureFlash : true
+  }));
+
+  app.get('/home', isAuthenticated, function(req, res){
+    res.render('home', { user: req.user });
+  });
+
+  app.get('/signout', function(req, res) {
+    req.logout();
+    res.redirect('/');
   });
 
   app.use('/categories', require('./routes/category'));
